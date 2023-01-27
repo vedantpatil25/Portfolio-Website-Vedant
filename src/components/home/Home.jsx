da8f377bd017136c2873a0501ab10f98e7584f24
vedantpatil <vedantpatil2002@gmail.com>
2023-01-28 01:05:03 +0530

    added home scrollig animatoin

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "./home.css";
 import Social from './Social'
 import Data from './Data';
 import ScrollDown from './ScrollDown';
+import Aos from 'aos';
+import "aos/dist/aos.css";
+
 const Home = () => {
+
+    useEffect(() => {
+        Aos.init({duration:2000});
+      },[]);
+
   return (
     <>
-    <section className='home section' id = 'home'>
+    <section className='home section' id = 'home' data-aos = 'zoom-out'>
         <div className="home__container container grid">
             <div className="home__content grid">
 
