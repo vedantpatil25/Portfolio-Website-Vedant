3aa5c1e8893a147a66ae677fbaee7dd4054b8adf
vedantpatil <vedantpatil2002@gmail.com>
2023-01-28 22:52:38 +0530

    updated scroll on each project item

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 const WorkItems = ({item}) => {
+
+  useEffect(() => {
+    Aos.init({duration:500});
+  },[]);
+
   return (
-    <div className='work__card' key = {item.id}>
+    <div className='work__card' data-aos ="fade-up" key = {item.id}>
         <img src={item.image} alt=""  className='work__img'/>
-        <h3 className="work__title">{item.title}</h3>
+        <h3 className="work__title" >{item.title}</h3>
         <a href="#" className="work__button">
             View Project <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
