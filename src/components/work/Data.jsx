ef4f2d97fa012955f910659c232226c7dae13637
vedantpatil <vedantpatil2002@gmail.com>
2023-01-30 01:21:31 +0530

    added projects in portfolio section

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,8 +1,9 @@
-import Work1 from "../../assets/work1.jpg"
-import Work2 from "../../assets/work2.jpg"
-import Work3 from "../../assets/work3.jpg"
-import Work4 from "../../assets/work4.jpg"
-import Work5 from "../../assets/work5.jpg"
+import Work1 from "../../assets/pr_food.png"
+import Work2 from "../../assets/pr_travel.png"
+import Work3 from "../../assets/pr_calculator.png"
+import Work4 from "../../assets/pr_lottery.jpg"
+import Work5 from "../../assets/pr_webpro.png"
+import Work6 from "../../assets/pr_form.png"
 
 
 
@@ -11,32 +12,50 @@ export const projectData =
     {
         id: 1,
         image: Work1,
-        title: "Web design",
+        title: "Restaurant Menu",
         category: "web",
+        tech : " React.js | CSS ",
+        link : "https://food-court-12b40.web.app/",
       },
       {
         id: 2,
         image: Work2,
-        title: "App movil",
+        title: "Travel Booking System",
         category: "app",
+        tech : " Java Swing | SQL | JDBC Connectivity ",
+        link : "https://github.com/vedantpatil25/Ticket-booking-system",
       },
       {
         id: 3,
         image: Work3,
-        title: "Brand design",
+        title: "Calculator",
         category: "design",
+        tech : " HTML | CSS | Javascript ",
+        link : "https://calculator-by-vedant.netlify.app/",
       },
       {
         id: 4,
         image: Work4,
-        title: "App movil",
+        title: " Lottery System |  Blockchain ",
         category: "app",
+        tech : " Solidity | React | Typescript",
+        link : "https://github.com/vedantpatil25/Blockchain-Lottery-System",
       },
       {
         id: 5,
         image: Work5,
-        title: "Web design",
+        title: "Courses Static Website",
         category: "web",
+        tech : " HTML | CSS ",
+        link : "https://webpro-courses.netlify.app/",
+      },
+      {
+        id: 6,
+        image: Work6,
+        title: "Survey Form",
+        category: "web",
+        tech : " HTML | CSS ",
+        link : "https://form-by-vedant.netlify.app",
       },
 ]
 
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -12,7 +12,8 @@ const WorkItems = ({item}) => {
     <div className='work__card' data-aos ="fade-up" key = {item.id}>
         <img src={item.image} alt=""  className='work__img'/>
         <h3 className="work__title" >{item.title}</h3>
-        <a href="#" className="work__button">
+        <span className="project_tech">{item.tech}</span>
+        <a href={item.link} className="work__button" target="_blank">
             View Project <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
     </div>
