8660f341c77e1153acd161efbf06401b2748eb2d
vedantpatil <vedantpatil2002@gmail.com>
2023-02-01 01:54:21 +0530

    added github links and word counter project

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -1,23 +1,24 @@
-import React,{useEffect} from 'react'
-import Aos from 'aos';
+import React, { useEffect } from "react";
+import Aos from "aos";
 import "aos/dist/aos.css";
 
-const WorkItems = ({item}) => {
-
+const WorkItems = ({ item }) => {
   useEffect(() => {
-    Aos.init({duration:500});
-  },[]);
+    Aos.init({ duration: 500 });
+  }, []);
 
   return (
-    <div className='work__card' data-aos ="fade-up" key = {item.id}>
-        <img src={item.image} alt=""  className='work__img'/>
-        <h3 className="work__title" >{item.title}</h3>
-        <span className="project_tech">{item.tech}</span>
-        <a href={item.link} className="work__button" target="_blank">
-            View Project <i className="bx bx-right-arrow-alt work__button-icon"></i>
-        </a>
-    </div>
-  )
-}
+    <div className="work__card" data-aos="fade-up" key={item.id}>
+      <img src={item.image} alt="" className="work__img" />
+      <h3 className="work__title">{item.title}</h3>
+      <span className="project_tech">{item.tech}</span>
 
-export default WorkItems
+      <a href={item.link} className="work__button" target="_blank">
+        View Project <i className="bx bx-right-arrow-alt work__button-icon"></i>
+      </a>
+      <a href={item.github} className="github__link work__button" target="_blank">Github Link &nbsp;&nbsp; <i class='bx bxl-github project__github-link'></i></a>
+    </div>
+  );
+};
+
+export default WorkItems;
