d163cb54636918af656ab85fc0578238f62814b0
vedantpatil <vedantpatil2002@gmail.com>
2023-01-31 20:02:30 +0530

    added counter to about section

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-
+import CountUp from 'react-countup'
 const Info = () => {
   return (
      <div className="about__info grid">
 
         <div className="about__box">
         <i class='bx bxl-github'></i>
-            <h2 className="about__title">10+</h2>
+            <h2 className="about__title"> <CountUp start={0} end={10} duration={4} delay ={0}/>+ </h2>
             <span className="about__subtitle">Projects</span>
 
         </div>
 
         <div className="about__box">
         <i class='bx bxl-c-plus-plus'></i>
-            <h3 className="about__title">500+</h3>
+            <h3 className="about__title"> <CountUp start={0} end={500} duration={4} delay ={0}/>+ </h3>
             <span className="about__subtitle">DSA Problems Solved</span>
 
         </div>
 
         <div className="about__box">
         <i class='bx bxs-certification' ></i>
-            <h3 className="about__title">10+</h3>
+            <h3 className="about__title"><CountUp start={0} end={10} duration={4} delay ={0}/>+</h3>
             <span className="about__subtitle">Certificates</span>
 
         </div>
